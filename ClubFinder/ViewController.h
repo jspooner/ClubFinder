24ca155dfb7c754154c72e1af46836eb354d391a
Jonathan Spooner <jspooner@gmail.com>
2014-02-20 21:39:51 -0800

    Added text area log

--- ClubFinder/ViewController.h
+++ ClubFinder/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FYX/FYXVisitManager.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <FYXVisitDelegate>
 
 @end
--- ClubFinder/ViewController.m
+++ ClubFinder/ViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <FYX/FYXVisitManager.h>
+#import <FYX/FYXTransmitter.h>
 
 @interface ViewController ()
-
+@property (nonatomic) FYXVisitManager *visitManager;
+@property (nonatomic) IBOutlet UITextView *textView;
 @end
 
 @implementation ViewController
@@ -17,6 +20,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.visitManager = [FYXVisitManager new];
+    self.visitManager.delegate = self;
+    [self.visitManager start];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +30,37 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma - mark
+#pragma - mark Helpers
+
+-(void)logMessage:(NSString*)message
+{
+    NSLog(message);
+    self.textView.text = [self.textView.text stringByAppendingString:[NSString stringWithFormat:@"\n%@", message]];
+}
+
+#pragma - mark
+#pragma - mark FYXVisitDelegate
+
+- (void)didArrive:(FYXVisit *)visit;
+{
+    // this will be invoked when an authorized transmitter is sighted for the first time
+    [self logMessage:[NSString stringWithFormat:@"I arrived at a Gimbal Beacon!!! %@", visit.transmitter.name]];
+}
+
+- (void)receivedSighting:(FYXVisit *)visit updateTime:(NSDate *)updateTime RSSI:(NSNumber *)RSSI;
+{
+    // this will be invoked when an authorized transmitter is sighted during an on-going visit
+    [self logMessage:[NSString stringWithFormat:@"I received a sighting!!! %@", visit.transmitter.name]];
+}
+
+- (void)didDepart:(FYXVisit *)visit;
+{
+    // this will be invoked when an authorized transmitter has not been sighted for some time
+    [self logMessage:[NSString stringWithFormat:@"I left the proximity of a Gimbal Beacon!!!! %@", visit.transmitter.name]];
+    [self logMessage:[NSString stringWithFormat:@"I was around the beacon for %f seconds", visit.dwellTime]];
 }
 
 @end
