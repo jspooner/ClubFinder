ae4253136957dd545bc599f93a4b22e1459ad731
Jonathan Spooner <jspooner@gmail.com>
2014-03-28 21:45:21 -0700

    Added app logging

--- ClubFinder/AppDelegate.m
+++ ClubFinder/AppDelegate.m
@@ -43,10 +43,12 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"[%f] log?e=app/applicationWillResignActive", [[NSDate date] timeIntervalSince1970]);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    NSLog(@"[%f] log?e=app/applicationDidEnterBackground", [[NSDate date] timeIntervalSince1970]);
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
@@ -54,29 +56,32 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"[%f] log?e=app/applicationWillEnterForeground", [[NSDate date] timeIntervalSince1970]);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"[%f] log?e=app/applicationDidBecomeActive", [[NSDate date] timeIntervalSince1970]);
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"[%f] log?e=app/applicationWillTerminate", [[NSDate date] timeIntervalSince1970]);
 }
 
 - (void)serviceStarted
 {
     // this will be invoked if the service has successfully started
     // bluetooth scanning will be started at this point.
-    NSLog(@"FYX Service Successfully Started");
+    NSLog(@"[%f] log?e=app/gimbal/serviceStarted", [[NSDate date] timeIntervalSince1970]);
 }
 
 - (void)startServiceFailed:(NSError *)error
 {
     // this will be called if the service has failed to start
-    NSLog(@"%@", error);
+    NSLog(@"[%f] log?e=app/gimbal/startServiceFailed&error=%@", [[NSDate date] timeIntervalSince1970], error);
 }
 
 @end
