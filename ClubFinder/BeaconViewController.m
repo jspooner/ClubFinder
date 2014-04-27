762d10aa03c1c4a667176f8b579cbf6988a2a84c
Jonathan Spooner <jspooner@gmail.com>
2014-04-27 11:31:08 -0700

    BVC is now updateding cells on beacon sightings

--- ClubFinder/BeaconViewController.m
+++ ClubFinder/BeaconViewController.m
@@ -52,7 +52,98 @@
 -(void)transmitterUpdated:(NSNotification *)notification
 {
     NSLog(@"+++++++++++++++++++++++++++++++++++++++++++++++  transmitterAdded %@", [[notification userInfo] objectForKey:@"index"]);
+    NSNumber *index = [[notification userInfo] objectForKey:@"index"];
+    int i = [index intValue];
+    Transmitter *transmitter = [self.beaconManager.transmitters objectAtIndex:[index intValue]];
+//    NSIndexPath *indexPath = [[notification userInfo] objectForKey:@"index"];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:i inSection:0];
+    for (UITableViewCell *cell in self.tableView.visibleCells) {
+        if ([[self.tableView indexPathForCell:cell] isEqual:indexPath]) {
+            SightingsTableViewCell *sightingsCell = (SightingsTableViewCell *)cell;
+
+            CALayer *tempLayer = [sightingsCell.rssiImageView.layer presentationLayer];
+            transmitter.previousRSSI =  [self rssiForBarWidth:[tempLayer frame].size.width];
+
+            [self updateSightingsCell:sightingsCell withTransmitter:transmitter];
+        }
+    }
+
 }
+
+- (NSNumber *)rssiForBarWidth:(float)barWidth
+{
+    NSInteger barMaxValue = -60; // [[NSUserDefaults standardUserDefaults] integerForKey:@"rssi_bar_max_value"];
+    NSInteger barMinValue = -90; // [[NSUserDefaults standardUserDefaults] integerForKey:@"rssi_bar_min_value"];
+    
+    NSInteger barRange = barMaxValue - barMinValue;
+    float percentage = - ((barWidth / 270.0f) - 1.0f);
+    float rssiValue = - ((percentage * (float)barRange) - barMaxValue);
+    
+    return [NSNumber numberWithFloat:rssiValue];
+}
+
+- (float)barWidthForRSSI:(NSNumber *)rssi
+{
+    NSInteger barMaxValue = -60; //[[NSUserDefaults standardUserDefaults] integerForKey:@"rssi_bar_max_value"];
+    NSInteger barMinValue = -90; // [[NSUserDefaults standardUserDefaults] integerForKey:@"rssi_bar_min_value"];
+    
+    float rssiValue = [rssi floatValue];
+    float barWidth;
+    if (rssiValue >= barMaxValue) {
+        barWidth = 270.0f;
+    } else if (rssiValue <= barMinValue) {
+        barWidth = 5.0f;
+    } else {
+        NSInteger barRange = barMaxValue - barMinValue;
+        float percentage = (barMaxValue - rssiValue) / (float)barRange;
+        barWidth = (1.0f - percentage) * 270.0f;
+    }
+    return barWidth;
+}
+
+
+- (void)updateSightingsCell:(SightingsTableViewCell *)sightingsCell withTransmitter:(Transmitter *)transmitter
+{
+    if (sightingsCell && transmitter) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            sightingsCell.contentView.alpha = 1.0f;
+            
+            float oldBarWidth = [self barWidthForRSSI:transmitter.previousRSSI];
+            float newBarWidth = [self barWidthForRSSI:transmitter.rssi];
+            CGRect tempFrame = sightingsCell.rssiImageView.frame;
+            CGRect oldFrame = CGRectMake(tempFrame.origin.x, tempFrame.origin.y, oldBarWidth, tempFrame.size.height);
+            CGRect newFrame = CGRectMake(tempFrame.origin.x, tempFrame.origin.y, newBarWidth, tempFrame.size.height);
+            
+            // Animate updating the RSSI indicator bar
+            sightingsCell.rssiImageView.frame = oldFrame;
+            [UIView animateWithDuration:1.0f animations:^{
+                sightingsCell.rssiImageView.frame = newFrame;
+            }];
+            sightingsCell.isGrayedOut = NO;
+            UIImage *batteryImage = [self getBatteryImageForLevel:transmitter.batteryLevel];
+            [sightingsCell.batteryImageView setImage:batteryImage];
+            sightingsCell.temperature.text = [NSString stringWithFormat:@"%@%@", transmitter.temperature,
+                                              [NSString stringWithUTF8String:"\xC2\xB0 F" ]];
+            sightingsCell.rssiLabel.text = [NSString stringWithFormat:@"%@", transmitter.rssi];
+            
+        });
+    }
+}
+
+- (UIImage *)getBatteryImageForLevel: (NSNumber *)batteryLevel
+{
+    switch([batteryLevel integerValue]){
+        case 0:
+        case 1:
+            return [UIImage imageNamed:@"battery_low.png"];
+        case 2:
+            return [UIImage imageNamed:@"battery_high.png"];
+        case 3:
+            return [UIImage imageNamed:@"battery_full.png"];
+    }
+    return [UIImage imageNamed:@"battery_unknown.png"];
+}
+
 
 //-(void)transmitterUpdated:(NSNotification *)notification
 //{
